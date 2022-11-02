12b403a08637745497b68e264e2aaee848ba7dac
cuonglx-1805 <luong.xuan.cuong@sun-asterisk.com>
2022-11-02 12:31:21 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+  
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CcmBiNanDiLenSeeder::class);
+        $this->call(CcmBiNanDiXuongSeeder::class);
+        $this->call(CcmTrinhSatDiLenSeeder::class);
+        $this->call(CcmTrinhSatDiXuongSeeder::class);
+        $this->call(OxiBiNanDiLenSeeder::class);
+        $this->call(OxiBiNanDiXuongSeeder::class);
+        $this->call(OxiTrinhSatDiLenSeeder::class);
+        $this->call(OxiTrinhSatDiXuongSeeder::class);
+    }
+}
